f1a0c10527c12a02e8511d8901a57a0df8115390
Michael Nolan <mkl.jhn.nln@gmail.com>
2022-11-28 10:38:29 -0500

    testing if i can make branch fine on cd/client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef, useContext } from "react";
 import Checkbox from "./Checkbox";
 import PlayBeatChord from "./PlayBeatChord";
+import Sequencer from "./Sequencer";
 // import { AudioEngineContext } from "./index.js";
 
 const audioContext = new AudioContext();
@@ -231,68 +232,7 @@ const App = () => {
     });
     // }
   };
-  return (
-    <div>
-      jello werld
-      <button
-        onClick={() => {
-          console.log("click");
-          if (!playing) {
-            console.log("play");
-            setPlaying(true);
-          } else {
-            setPlaying(false);
-          }
-        }}
-      >
-        start/stop
-      </button>
-      {/* <button onClick={() => synth.stop()}>stop synth</button> */}
-      <span>Tempo</span>
-      <input
-        type="range"
-        min="30.0"
-        max="160.0"
-        step="1"
-        value={tempo}
-        onInput={(e) => parseTempo(e)}
-      />{" "}
-      <span>{tempo}</span>
-      <section>
-        {
-          // can't map an object
-          areBeatsCheckedArray.map((chord) => {
-            // this first map with a div controls the note over time, i.e. the row
-            const chordIndex = chord;
-            return (
-              <div key={`row-${chordIndex}`}>
-                {areBeatsChecked[`chord-${chordIndex}`].map((check, index) => {
-                  return (
-                    <Checkbox
-                      key={`row-${chordIndex}-beat-${index}`}
-                      chord={chord}
-                      areBeatsChecked={areBeatsChecked}
-                      beatIndex={index}
-                      chordIndex={chordIndex}
-                      handleBeatCheckbox={handleBeatCheckbox}
-                    />
-                  );
-                })}
-              </div>
-            );
-          })
-        }
-      </section>
-      <button
-        onClick={() => {
-          console.log(playing);
-          console.log(areBeatsChecked);
-        }}
-      >
-        checked state
-      </button>
-    </div>
-  );
+  return <Sequencer />;
 };
 
 export default App;
--- client/src/Sequencer.js
+++ client/src/Sequencer.js
@@ -1,5 +1,66 @@
 const Sequencer = () => {
-    return (  );
-}
- 
+  return (
+    <div>
+      jello werld
+      <button
+        onClick={() => {
+          console.log("click");
+          if (!playing) {
+            console.log("play");
+            setPlaying(true);
+          } else {
+            setPlaying(false);
+          }
+        }}
+      >
+        start/stop
+      </button>
+      {/* <button onClick={() => synth.stop()}>stop synth</button> */}
+      <span>Tempo</span>
+      <input
+        type="range"
+        min="30.0"
+        max="160.0"
+        step="1"
+        value={tempo}
+        onInput={(e) => parseTempo(e)}
+      />{" "}
+      <span>{tempo}</span>
+      <section>
+        {
+          // can't map an object
+          areBeatsCheckedArray.map((chord) => {
+            // this first map with a div controls the note over time, i.e. the row
+            const chordIndex = chord;
+            return (
+              <div key={`row-${chordIndex}`}>
+                {areBeatsChecked[`chord-${chordIndex}`].map((check, index) => {
+                  return (
+                    <Checkbox
+                      key={`row-${chordIndex}-beat-${index}`}
+                      chord={chord}
+                      areBeatsChecked={areBeatsChecked}
+                      beatIndex={index}
+                      chordIndex={chordIndex}
+                      handleBeatCheckbox={handleBeatCheckbox}
+                    />
+                  );
+                })}
+              </div>
+            );
+          })
+        }
+      </section>
+      <button
+        onClick={() => {
+          console.log(playing);
+          console.log(areBeatsChecked);
+        }}
+      >
+        checked state
+      </button>
+    </div>
+  );
+};
+
 export default Sequencer;
